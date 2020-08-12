9a4ca5fd54e59321692e18c087297e625a1f37e3
Julian Higuera <higuera.julian@gmail.com>
2020-08-12 01:22:22 -0500

    A lot of changes

--- js/jsmain.js
+++ js/jsmain.js
@@ -2,6 +2,12 @@
 
 	var clientname;
 	var actualuserid;
+	var idtype;
+	
+	
+	function setid(id){
+		idtype=id;
+	}
 	
 	
 	function postnewuser3(){
@@ -35,7 +41,13 @@
 	});
 	}
 	
+	function cleartokens(){
+		window.localStorage.clear();
+	}
+	
 	function login(){
+		
+		cleartokens();
 		
 		var logindata = new FormData();
 		
@@ -46,6 +58,7 @@
 		axios.post('http://itsecurityassistantapi-dev.us-east-1.elasticbeanstalk.com/api/token/', logindata)
 		
 		.then(function (response) {
+		
 		console.log(response);
 		
 		
@@ -55,27 +68,22 @@
 		window.localStorage.setItem('accesstoken', response.data.access);
 		window.localStorage.setItem('refreshtoken', response.data.refresh);
 		
-		console.log(window.localStorage.getItem('accesstoken'))
-		console.log(window.localStorage.getItem('refreshtoken'))
+		//console.log(window.localStorage.getItem('accesstoken'))
+		//console.log(window.localStorage.getItem('refreshtoken'))
 		
 		//window.location="index.html";
 		
-	
-	
-		})
-		
-		.catch(function (error) {
-		console.log(error);
-	
-		});	
-		
-		
 		var token = window.localStorage.getItem('accesstoken') ;
+		console.log('token decoded ' + token);
 
 		var decoded = jwt_decode(token);
-		console.log(decoded);
+		console.log('token decoded ' + decoded);
 		
 		var usertype=decoded.type;
+		
+		window.localStorage.setItem('tipousuario', usertype);
+		
+		setid(usertype);
 		
 			
 		//SuperUser
@@ -87,7 +95,7 @@
 		
 		
 		var iscustomer=usertype.localeCompare('Customer');
-		var issuperuser=usertype.localeCompare('SuperUser');
+		var issuperuser=usertype.localeCompare('Super User');
 		var isauditor=usertype.localeCompare('Auditor');
 		var isanalyst=usertype.localeCompare('Analyst');
 		
@@ -97,14 +105,30 @@
 			window.location="index.html";
 		}
 		if(issuperuser==0){
-			//code for su
+			window.location="supanel.html";
 		}
 		if(isauditor==0){
 			//code for auditor
+			window.location="auditordashboard.html";
 		}
 		if(isanalyst==0){
 			//code for analyst
+			window.location="analystdashboard.html";
 		}
+		
+		
+		
+		
+	
+		})
+		
+		.catch(function (error) {
+		console.log(error);
+	
+		});	
+		
+		
+		
 		
 		
 		
@@ -287,11 +311,35 @@
 				var cell2 = row.insertCell(1);
 				var cell3 = row.insertCell(2);
 				var cell4 = row.insertCell(3);
+				var cell5 = row.insertCell(4);
+				var cell6 = row.insertCell(5);
+				var cellInstruction = row.insertCell(6);
+
+
+				var comparator=responsedata.data[i].state.name.localeCompare('Finished');
 				
+				if(comparator==0){
+
 				cell1.innerHTML = responsedata.data[i].id;
 				cell2.innerHTML = responsedata.data[i].date;
 				cell3.innerHTML = responsedata.data[i].price;
 				cell4.innerHTML = responsedata.data[i].description;
+				cell5.innerHTML = responsedata.data[i].state.name;
+				
+				cellInstruction.innerHTML = '<button class="btn btn-primary btn-xs my-xs-btn" type="button" onClick="changeRec(".$num.")" >'
+   				 + '<span class="glyphicon glyphicon-pencil"></span> Edit</button>';
+				
+				}
+
+				else {
+
+				cell1.innerHTML = responsedata.data[i].id;
+				cell2.innerHTML = responsedata.data[i].date;
+				cell3.innerHTML = responsedata.data[i].price;
+				cell4.innerHTML = responsedata.data[i].description;
+				cell5.innerHTML = responsedata.data[i].state.name;
+
+				}
 			
 		
 		}
@@ -312,6 +360,322 @@
 		
 		
 	}
+	
+	
+	function employestart(){
+		loaddataemployes();
+		getmeetsemploye();
+	}
+
+	function analyststart(){
+		loaddataemployes();
+
+	}
+	
+	
+	
+	function createemployer(){
+		
+		var today = new Date();
+		var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+		var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+		var dateTime = date+'T'+time;
+		
+		var sendtoken='Bearer '+window.localStorage.getItem('accesstoken') ;
+		
+		var imagen = $('#employerimageprofile')[0].files[0];
+
+		var employerdata = new FormData();
+	
+		employerdata.append("first_name", document.getElementById('employername').value);
+		employerdata.append("last_name", document.getElementById('employerlastname').value);
+		employerdata.append("username", document.getElementById('employerusername').value);
+		employerdata.append("password", document.getElementById('employerpass').value);
+		employerdata.append("email", document.getElementById('employermail').value);
+		employerdata.append("image", imagen);
+		employerdata.append("phone", document.getElementById('employerphone').value);
+		employerdata.append("identification.id", document.getElementById('employerid').value);
+		employerdata.append("staff_profile.join_date", dateTime);
+		employerdata.append("staff_profile.position", document.getElementById('jobselect').value);
+		
+		var url;
+		
+		var employertype= document.getElementById('jobselect').value;
+		
+		if(employertype==2){
+			//analista
+			url='http://itsecurityassistantapi-dev.us-east-1.elasticbeanstalk.com/api/Analysts/';
+			
+		}
+		if(employertype==3){
+			//auditor
+			url='http://itsecurityassistantapi-dev.us-east-1.elasticbeanstalk.com/api/Auditors/';
+		}
+		
+		console.log(url);
+
+	
+		axios.post(url, employerdata,{
+				headers: {
+
+			'Authorization': sendtoken,
+			
+		}
+		})
+		
+		
+		.then(function (response) {
+		
+		console.log(response);
+	
+		
+		})
+		
+		.catch(function (error) {
+		console.log(error);
+		});
+			
+	}
+	
+	
+	
+	function loaddataemployes(){
+		
+		
+		
+		
+		
+		var token = window.localStorage.getItem('accesstoken') ;
+
+		var decoded = jwt_decode(token);
+		console.log(decoded);
+		
+		useractualid=decoded.user_id;
+		actualuserid=decoded.user_id;
+		
+		//console.log(useractualid);
+		
+		var cargo=window.localStorage.getItem('tipousuario') ;
+		
+		console.log(cargo);
+		
+		
+		var urlstring= null;
+		
+		//http://itsecurityassistantapi-dev.us-east-1.elasticbeanstalk.com/api/Analysts/
+		//http://itsecurityassistantapi-dev.us-east-1.elasticbeanstalk.com/api/Auditors/
+		
+		
+		
+		if(cargo.localeCompare('Auditor')==0){
+			urlstring= 'http://itsecurityassistantapi-dev.us-east-1.elasticbeanstalk.com/api/Auditors/'+useractualid+'/';
+		}
+		
+		if(cargo.localeCompare('Analyst')==0){
+			urlstring= 'http://itsecurityassistantapi-dev.us-east-1.elasticbeanstalk.com/api/Analysts/'+useractualid+'/';
+		}
+
+		
+		//SuperUser
+		//Analyst
+		//Auditor
+		//Customer
+		
+		//console.log(urlstring);
+		
+		var sendtoken='Bearer '+window.localStorage.getItem('accesstoken') ;
+		
+		axios.get(urlstring, {
+			headers: {
+			'Authorization': sendtoken
+		}
+		})
+		
+		.then(function (response) {
+		
+		//console.log(response);
+		
+		clientname=response.data.first_name+' '+response.data.last_name;
+		
+		//console.log(clientname);
+		
+		
+		javascript:document.getElementById('visualemployename').innerHTML=clientname;
+		
+		document.getElementById("employerprofilepic").src=response.data.image;
+		
+		
+		})
+		
+		.catch(function (error) {
+		console.log(error);
+		});
+		
+		
+		}
+		
+		
+		
+		
+		
+	
+		function getmeetsemploye(){
+		
+
+		
+
+		
+		var globalresponse=null;
+		
+		var getmeeturl= 'http://itsecurityassistantapi-dev.us-east-1.elasticbeanstalk.com/api/PendingMeetings/';
+		
+		
+		var sendtoken='Bearer '+window.localStorage.getItem('accesstoken') ;
+		
+		axios.get(getmeeturl, {
+			headers: {
+			'Authorization': sendtoken
+		}
+		})
+		
+		.then(function (response) {
+			
+		window.localStorage.setItem('meetsize', response.data.length);
+		
+		
+		console.log(response);
+		
+		
+		
+	
+		tabledef2(response);
+		
+		
+		
+		})
+		
+		.catch(function (error) {
+		console.log(error);
+		});
+		
+		
+		
+		
+	
+		var table2 = document.getElementById("auditortable");
+		
+		var tablesize=window.localStorage.getItem('meetsize');
+		
+		function tabledef2(object2){
+			
+		
+		var responsedata2=object2;
+		
+		
+		for (var i = 0; i < tablesize; i++) {
+			
+			/*orden en la tabla
+			
+			auditor->auditor
+			id cliente->customer
+			fecha->date
+			description->description
+			id reunion->id
+			tipo reunion->meeting_class
+			clase reunion->meeting_type
+			precio->price
+			estado->state
+			
+			*/
+				
+		
+	
+				var row = table2.insertRow(i+1);
+				
+				var cell1 = row.insertCell(0);
+				var cell2 = row.insertCell(1);
+				var cell3 = row.insertCell(2);
+				var cell4 = row.insertCell(3);
+				var cell5 = row.insertCell(4);
+				var cell6 = row.insertCell(5);
+				var cell7 = row.insertCell(6);
+				var cell8 = row.insertCell(7);
+				var cell9 = row.insertCell(8);
+			
+				
+				cell1.innerHTML = responsedata2.data[i].auditor;
+				cell2.innerHTML = responsedata2.data[i].customer;
+				cell3.innerHTML = responsedata2.data[i].date;
+				cell4.innerHTML = responsedata2.data[i].description;
+				cell5.innerHTML = responsedata2.data[i].id;
+				cell6.innerHTML = responsedata2.data[i].meeting_class.name;
+				cell7.innerHTML = responsedata2.data[i].meeting_type.name;
+				cell8.innerHTML = responsedata2.data[i].price;
+				cell9.innerHTML = responsedata2.data[i].state.name;
+				
+				
+				var cellInstruction = row.insertCell(9);
+				cellInstruction.innerHTML = '<button class="btn btn-primary btn-xs my-xs-btn" type="button" onClick="Auditar('+responsedata2.data[i].id+')" >'
+				+ '<span class="glyphicon glyphicon-pencil"></span> Auditar</button>';
+			
+			
+		
+		}
+		
+		var rowtodelete=parseInt(tablesize)+1;
+		
+		document.getElementById("auditortable").deleteRow(rowtodelete);
+		
+		
+		}
+		
+	}
+	
+	
+	function Auditar(meetingid){
+
+		var saver=meetingid;
+		
+		var sendtoken='Bearer '+window.localStorage.getItem('accesstoken') ;
+
+		var auditurl='http://itsecurityassistantapi-dev.us-east-1.elasticbeanstalk.com/api/PendingMeetings/'+meetingid+'/';
+
+		var payload = {"auditor": actualuserid, "state": 4};
+
+		axios.patch(auditurl, payload,{
+			headers: {
+
+			'Authorization': sendtoken,
+			
+		}
+		})
+		
+		.then(function (response) {
+		console.log(response);
+	
+	
+		})
+	
+		.catch(function (error) {
+		console.log(error);
+
+		});
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+	
+	
 		
 		
 		
