058769b431053acc3f5b9ebb71d3ee8dfa6ab620
Julian Higuera <higuera.julian@gmail.com>
2020-08-12 05:09:01 -0500

    xxx

--- js/jsmain.js
+++ js/jsmain.js
@@ -371,7 +371,7 @@
 				cell4.innerHTML = responsedata.data[i].description;
 				cell5.innerHTML = responsedata.data[i].state.name;
 				
-				cellInstruction.innerHTML = '<button class="btn btn-primary btn-xs my-xs-btn" type="button" onClick="changeRec(".$num.")" >'
+				cellInstruction.innerHTML = '<button class="btn btn-primary btn-xs my-xs-btn" type="button" onClick="" >'
    				 + '<span class="glyphicon glyphicon-pencil"></span> Edit</button>';
 				
 				}
@@ -764,6 +764,7 @@ function createReport(){
 	
 		.catch(function (error) {
 		console.log(error);
+		console.log(error.response.data);
 
 		});
 
